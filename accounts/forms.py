012b0560824244adc3f78b6d40f88569346adb81
Hforna <henriqueflashpingo@gmail.com>
2024-07-18 17:33:14 -0300

    forms input not exclude fixed

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,23 @@
+from typing import Any, Mapping
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
+
+
+def add_attr(field_name, widget_attr, text_in_widget):
+    existing = field_name.widget.attrs.get(widget_attr, "")
+    field_name.widget.attrs[widget_attr] = f'{existing} {text_in_widget}'.strip()
 
 class FormUser(forms.ModelForm):
-    password2 = forms.CharField(max_length=256, required=False)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_attr(self.fields["username"], "placeholder", "Username")
+
+    password = forms.CharField(widget=forms.PasswordInput, max_length=256, required=False)
+    password2 = forms.CharField(widget=forms.PasswordInput, max_length=256, required=False)
 
     class Meta:
         model = User
@@ -20,7 +34,17 @@ class FormUser(forms.ModelForm):
         'username': {'required': 'Username is required', 'max_length': 'The username is very long'}
     }
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists()
 
+        if exists:
+            raise ValidationError(
+                'User e-mail is already in use', code='invalid',
+            )
+
+        return email
+    
     def clean(self):
         cleaned_data = super().clean()
         password1 = cleaned_data.get('password')
--- accounts/views.py
+++ accounts/views.py
@@ -8,15 +8,19 @@ from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from django.views import View
 from .forms import FormUser
-from django.http import Http404
+from django.http import Http404, HttpResponse
 
 
 # Create your views here.
 
 class SignUp(View):
     def get(self, request):
-        get_data_user_form = request.session.get("get_data_form", None)
-        form = FormUser(get_data_user_form)
+        if request.session.get("before_in_create_page", False):
+            get_data_user_form = request.session.get("get_data_form", None)
+            form = FormUser(get_data_user_form)
+            request.session["before_in_create_page"] = False
+        else:
+            form = FormUser()
         return render(request, "accounts/signup.html", context={'form': form})
 
 
@@ -26,11 +30,13 @@ def create_user(request):
     
     form = FormUser(request.POST)
     request.session["get_data_form"] = request.POST
+    request.session["before_in_create_page"] = True
     if form.is_valid():
         post = form.save(commit=False)
         post.set_password(post.password)
         post.save()
         request.session["get_data_form"] = None
+        request.session["before_in_create_page"] = False
         return redirect("/accounts/login")
     return redirect("/accounts/signup")
 
@@ -51,8 +57,5 @@ def login_page(request):
 
 @login_required(login_url="/accounts/login", redirect_field_name="next")
 def logouts(request):
-    if not request.POST:
-        return Http404
-    
     logout(request)
     return redirect("/accounts/login")
